94b30be0f07ef2acd86c87670f85f535ebceeacf
pdouglasjr <phillip.duane.douglas.jr@gmail.com>
2021-02-26 21:06:17 -0500

    Final version

---
+++ block_letters/initials.py
@@ -0,0 +1,19 @@
+# Name:     initials.py
+# Author:   Phillip Douglas
+
+name = "Phillip Douglas"
+
+print(f"My name is {name} and I want to be a Pythonista!")
+print("\nMy initials are...")
+
+initials = """
+P P P P     D D D D
+P       P   D       D
+P       P   D       D
+P P P P     D       D
+P           D       D
+P           D       D
+P           D D D D
+"""
+
+print(initials)
