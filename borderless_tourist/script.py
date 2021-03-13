3d371619117d6715a0efd7f180beb13c05caee29
pdouglasjr <phillip.duane.douglas.jr@gmail.com>
2021-03-13 16:19:50 -0500

    Added test objects

--- borderless_tourist/script.py
+++ borderless_tourist/script.py
@@ -1,2 +1,9 @@
 # Name:     borderless_tourist/script.py
 # Author:   Phillip Douglas
+
+# Destinations
+destinations = ["Paris, France","Shanghai,"China","Los Angeles, USA","São Paulo, Brazil"]
+
+# Test traveler
+test_traveler = ["Erin Wilkes","Shanghai, China",["historical site", "art"]]
+
